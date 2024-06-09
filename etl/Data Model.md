9934306285b1714f715be46ffe4dadee666be0ef
Ismael Miranda <ismaelmiranda11@hotmail.com>
2024-06-09 18:14:51 +0100

    ddl script create

--- etl/Data Model.md
+++ etl/Data Model.md
@@ -1,4 +1,4 @@
-# Data Model - IBGE Brazil Inflation 
+# Data Model - IBGE Brazil Inflation
 
 ## Goal 🎯
 
@@ -10,9 +10,9 @@ IBGE (Instituto Brasileiro de Geografia e Estatística) is a public institution 
 
 The [API](https://servicodados.ibge.gov.br/api/docs/) provides a lot of information. Here, the work will concentrate on the [aggregate indicators data](https://servicodados.ibge.gov.br/api/docs/agregados?versao=3#api-bq).
 
-Among availables aggregates, the project will focus on inflation data (IPCA - Indice Nacional de Preços ao Consumidor Amplo). 
+Among availables aggregates, the project will focus on inflation data (IPCA - Indice Nacional de Preços ao Consumidor Amplo).
 
-More specifically, the project will focus on the inflation data from 2020 onwards. In API, the code for this aggregate is `7060 - IPCA - Variação mensal, acumulada no ano, acumulada em 12 meses e peso mensal, para o índice geral, grupos, subgrupos, itens e subitens de produtos e serviços (a partir de janeiro/2020)`.  
+More specifically, the project will focus on the inflation data from 2020 onwards. In API, the code for this aggregate is `7060 - IPCA - Variação mensal, acumulada no ano, acumulada em 12 meses e peso mensal, para o índice geral, grupos, subgrupos, itens e subitens de produtos e serviços (a partir de janeiro/2020)`.
 Yeah, it's a long name in Portuguese, but it means that the data is about the IPCA index, with monthly variation, accumulated variation within the year, accumulated variation in 12 months, and monthly weight for the general index, groups, subgroups, items, and subitems of products and services.
 
 The API works with a URL structure like this:
@@ -34,7 +34,7 @@ Thus, there are available 3 parameters to retrieve the data:
 
 The others parameters are fixed: `variaveis` and `classificacao`
 
-The other parameters, `variaveis` (variables) and `classificacao` (classification), are fixed.  
+The other parameters, `variaveis` (variables) and `classificacao` (classification), are fixed.
 
 #### Variables
 
@@ -42,7 +42,7 @@ For the aggregate, there's differents variables.
 
 Variables are the interest numbers for this project.
 
-Four will be treated: 
+Four will be treated:
 - `63 - IPCA - Variação mensal (%)` : Prices variation by month.
 - `69 - IPCA - Variação acumulada no ano (%)`: Accumalate variation within the year.
 - `2265 - IPCA - Variação acumulada em 12 meses (%)`: Accumulate variation in 12 running months.
@@ -50,7 +50,7 @@ Four will be treated:
 
 #### Classification and their categories
 
-Classification stores the categories granularity of the data. 
+Classification stores the categories granularity of the data.
 
 Categories for inflation means the groups of products and services that are used to calculate the inflation index. For example, food, transportation, housing, etc. They are divided in levels. House is a level 1 category, and inside it, there are more categories, like rent, electricity, etc.
 
@@ -58,7 +58,7 @@ The main one is `Índice Geral` the only one that has level 0. This represents t
 
 The avaible categories id and names are also in the API, in the aggregate metadata.
 
-For the project, with `7060` aggregate, the categories data is in: 
+For the project, with `7060` aggregate, the categories data is in:
 `'https://servicodados.ibge.gov.br/api/v3/agregados/7060/metadados'`, in the section classificacao -> categorias.
 
 
@@ -96,10 +96,12 @@ For each Airflow data interval period:
 The cities are the first information to take from API. The id of each city is used in the API url to retrieve data.
 
 Table: `cities`
-| Colunm | Description |
-| --- | --- |
-| id | City id |
-| name | City name |
+Colunm | Description
+--- | ---
+city_id | City id
+name | City name
+cretead_at | The date of the city insertion
+updated_at | The date of the city update
 
 #### Categories 📚
 
@@ -109,10 +111,12 @@ Table: `categories`
 
 Columns | Description
 --- | ---
-id | Category id
+category_id | Category id
 id_name | Category name with id
 name | Category name
 level | Category level
+created_at | The date of the category insertion
+updated_at | The date of the category update
 
 #### Calendar 📅
 
@@ -126,8 +130,10 @@ Columns | Description
 --- | ---
 date | The date
 month | The month
+month_name | The month name
+month_abbr | The month abbreviation
 year | The year
-quarter | The quarter
+
 ### Raw Tables 📊
 
 #### Raw JSON Data
@@ -147,7 +153,7 @@ api_url | The url used to retrieve the data
 created_at | The date of the data retrieval
 updated_at | The date of the data update
 
-### Fact Table 
+### Fact Table
 
 #### Inflation 📈
 
---
+++ etl/ddl.py
@@ -0,0 +1,180 @@
+'''Definition of tables of the project
+
+This module contains the definition of the tables used in the project.
+The tables are defined using the SQLAlchemy ORM.
+
+The target database is PostgreSQL, running in a local Docker container.
+
+'''
+from sqlalchemy import Column
+from sqlalchemy import PrimaryKeyConstraint
+from sqlalchemy import create_engine, inspect
+from sqlalchemy import text
+
+from sqlalchemy.orm import declarative_base
+
+from sqlalchemy.dialects import postgresql
+
+# Define the Base class
+Base = declarative_base()
+
+# Define the tables
+
+class cities(Base):
+    '''Dimensional Cities table
+
+    The cities are the first information to take from API.
+    The id of each city is used in the API url to retrieve data.
+
+    '''
+
+    __tablename__ = 'cities'
+
+    city_id = Column(type_=postgresql.INTEGER, primary_key=True, comment='City id')
+    city_name = Column(type_=postgresql.VARCHAR(100), comment='City name')
+
+    created_at = Column(type_=postgresql.TIMESTAMP,
+                       server_default=text("(NOW())"),
+                       comment='The date of the city insertion')
+    updated_at = Column(type_=postgresql.TIMESTAMP,
+                       server_default=text("(NOW())"),
+                       onupdate=text("(NOW())"),
+                       comment='The date of the city update')
+
+class categories(Base):
+    '''Categories dimension table
+
+    The categories are retrieved from the metadata.
+
+    '''
+
+    __tablename__ = 'categories'
+
+    category_id = Column(type_=postgresql.INTEGER, primary_key=True,
+                         comment='Category id')
+    category_name = Column(type_=postgresql.VARCHAR(2000),
+                           comment='Category name')
+    level = Column(type_=postgresql.INTEGER, comment='Category level')
+
+    created_at = Column(type_=postgresql.TIMESTAMP,
+                        server_default=text("(NOW())"),
+                        comment='The date of the category insertion')
+    updated_at = Column(type_=postgresql.TIMESTAMP,
+                        server_default=text("(NOW())"),
+                        onupdate=text("(NOW())"),
+                        comment='The date of the category update')
+
+class calendar(Base):
+    '''Calendar dimension table
+
+    An helper table to be used in data analysis.
+
+    '''
+
+    __tablename__ = 'calendar'
+
+    date = Column(type_=postgresql.DATE, primary_key=True, comment='The date')
+    month = Column(type_=postgresql.INTEGER, comment='The month')
+    month_name = Column(type_=postgresql.VARCHAR(20), comment='The month name')
+    month_abbr = Column(type_=postgresql.VARCHAR(3),
+                        comment='The month abbreviation')
+    year = Column(type_=postgresql.INTEGER, comment='The year')
+
+class raw_data_ibge(Base):
+    '''Raw table to store interval data from API
+
+    This table will be a type I, with an update of row in case of updates.
+
+    '''
+    __tablename__ = 'raw_data_ibge'
+
+    month_id = Column(type_=postgresql.INTEGER,
+                      comment='The month of the data')
+    city_id = Column(type_=postgresql.INTEGER,
+                     comment='The city id')
+    aggregate_id = Column(type_=postgresql.INTEGER,
+                          comment='The aggregate id')
+    aggregate_name = Column(type_=postgresql.VARCHAR(100),
+                            comment='The aggregate name')
+    json_data = Column(type_=postgresql.JSONB,
+                       comment='The data in json format')
+    api_url = Column(type_=postgresql.VARCHAR(2000),
+                     comment='The url used to retrieve the data')
+
+    created_at = Column(type_=postgresql.TIMESTAMP,
+                        comment='The date of the data retrieval',
+                        server_default=text("(NOW())"))
+    updated_at = Column(type_=postgresql.TIMESTAMP,
+                        comment='The date of the data update',
+                        server_default=text("(NOW())"),
+                        onupdate=text("(NOW())"))
+
+    # Define the primary key
+    __table_args__ = (
+        PrimaryKeyConstraint('month_id', 'city_id'),
+    )
+
+    def columns(self):
+        return [c.name for c in self.__table__.columns if not c.primary_key] #type:ignore
+
+    def fields_columns(self):
+        return [f'"{c}"' for c in self.columns()]
+
+
+class inflation(Base):
+    '''Inflation Fact table
+
+    The fact table will store the data from the API.
+
+    '''
+
+    __tablename__ = 'inflation'
+
+    month_id = Column(type_=postgresql.INTEGER,
+                      comment='The month of the data')
+    city_id = Column(type_=postgresql.INTEGER,
+                     comment='The city id')
+    category_id = Column(type_=postgresql.INTEGER,
+                         comment='The category id')
+    ipca_month_weight = Column(type_=postgresql.NUMERIC,
+                               comment='The category weight in the month')
+    ipca_month_variation = Column(type_=postgresql.NUMERIC,
+                                  comment='Prices variation by month')
+    ipca_accumulated_year_variation = Column(type_=postgresql.NUMERIC,
+                        comment='Accumalate variation within the year')
+    ipca_accumulated_12_months_variation = Column(type_=postgresql.NUMERIC,
+                        comment='Accumulate variation in 12 running months')
+
+    # Define the primary key
+    __table_args__ = (
+        PrimaryKeyConstraint('month_id', 'city_id', 'category_id'),
+    )
+
+
+def create_tables(Base, engine):
+    '''Create all tables in Base metadata
+
+    This function will create all tables in the Base metadata.
+
+    First, it will check if the table exists in the database.
+    If not, it will create the table.
+
+    '''
+    inspector = inspect(engine)
+
+    for table in Base.metadata.tables.values():
+        if not inspector.has_table(table.name): #type:ignore
+            table.create(engine)
+
+
+if __name__ == '__main__':
+
+    # Define the engine
+    engine = create_engine(
+        'postgresql+psycopg2://'
+        'postgres:postgres123@'
+        'localhost:5432'
+    )
+
+    # Create the tables
+    create_tables(Base, engine)
